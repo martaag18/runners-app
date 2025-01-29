bad668a06dc404f493577145933a023c7af978e5
Marta Altimir <martaag18@gmail.com>
2025-01-29 19:17:18 +0100

    Enabled CORS and added global validation pipe in NestJS backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,10 +4,19 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
-  console.log(`Application is running on: http://localhost:3000`);
+
+  // Habilitar CORS para permitir solicitudes de dominios específicos
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  });
+
   // Usa ValidationPipe globalmente para validar todos los DTOs
   app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(process.env.PORT || 3000);
+  console.log(`Application is running on: http://localhost:3000`);
 }
 
 bootstrap().catch((err) => {
