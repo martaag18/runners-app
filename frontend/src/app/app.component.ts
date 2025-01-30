0ab3784d234f292053f1d2a60f38fd97dfe8af83
Marta Altimir <martaag18@gmail.com>
2025-01-30 17:34:15 +0100

    Updates

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './core/navbar/navbar.component';
 import { FooterComponent } from './core/footer/footer.component';
+import { environment } from '../environments/environment'; // Importa el archivo de entorno
+import { isPlatformBrowser } from '@angular/common'; // Importa isPlatformBrowser
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'runners-app';
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {} //asegurar que código se está ejecutando en navegador
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.loadGoogleMapsScript();
+    }
+  }
+
+  // Método para cargar el script de Google Maps dinámicamente
+  loadGoogleMapsScript(): void {
+    const script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${environment.googleMapsApiKey}&libraries=places`;
+    script.async = true;
+    script.defer = true;
+    document.head.appendChild(script);
+  }
 }
+
+
