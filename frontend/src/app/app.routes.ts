32659a52d91607315bc7278127c6589dfe9ab67f
Marta Altimir <martaag18@gmail.com>
2025-02-06 12:39:04 +0100

    Created SignUp Component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { MapComponent } from './pages/map/map.component';
 import { CalendarComponent } from './pages/calendar/calendar.component';
 import { ChartComponent } from './pages/chart/chart.component';
+import { SignupComponent } from './pages/authentication/signup/signup.component';
 
 export const routes: Routes = [
     { path: "", component: HomeComponent},
     { path: "map", component: MapComponent},
     { path: "calendar", component: CalendarComponent},
     { path: "charts", component: ChartComponent},
+    { path:"signup", component: SignupComponent }
 ];
--- frontend/src/app/pages/authentication/signup/signup.component.ts
+++ frontend/src/app/pages/authentication/signup/signup.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
 
+  registerForm = new FormGroup({
+    name: new FormControl("", [Validators.required]),
+    surname: new FormControl("", [Validators.required]),
+    email: new FormControl("", [Validators.required, Validators.email]),
+    password: new FormControl("", [Validators.required]),
+    repeatPassword: new FormControl("", [Validators.required]),
+  })
+
+  submitRegister(){
+
+  }
 }
