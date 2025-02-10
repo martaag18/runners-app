7acb41827ff77061ca42bb695fb7d8e7645cfcf8
Marta Altimir <martaag18@gmail.com>
2025-02-10 18:40:56 +0100

    Uploads for deployment

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: true,
-    googleMapsApiKey: process.env['GOOGLE_MAPS_API_KEY'] || ''
-  };
-  
-console.log('Google Maps API Key (prod):', environment.googleMapsApiKey);
