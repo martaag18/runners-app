242710ab15cf71af3fb3b422771ceaa87d1291ed
Marta Altimir <martaag18@gmail.com>
2025-02-11 12:45:08 +0100

    Fixed errors in frontend

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
     googleMapsApiKey: 'AIzaSyBoitwdYbHbH0BRwtcswB5lPVTrSR-ozQA',
-    apiUrl: "https://runners-app-production.up.railway.app/"
-};
+    apiUrl: "https://runners-app-production.up.railway.app"
+  };
+  
