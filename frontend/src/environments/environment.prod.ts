d4f4bc85e4e3a22d2366a3755346a2169c44bc35
Marta Altimir <martaag18@gmail.com>
2025-02-11 12:08:05 +0100

    Connecting frontend with backend

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    googleMapsApiKey: 'AIzaSyBoitwdYbHbH0BRwtcswB5lPVTrSR-ozQA',
+    apiUrl: process.env['NEXT_PUBLIC_API_URL'] || 'https://fallback-url.com'
+};
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,5 @@
 export const environment = {
     production: false,
     googleMapsApiKey: 'AIzaSyBoitwdYbHbH0BRwtcswB5lPVTrSR-ozQA',
-    apiUrl: typeof process !== 'undefined' && process.env['NEXT_PUBLIC_API_URL']
-        ? process.env['NEXT_PUBLIC_API_URL']
-        : 'http://localhost:3000'  // Solo se usa en local
+    apiUrl: 'http://localhost:3000'
 };
