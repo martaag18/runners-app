f0cb47427735888ea94590a3511f1dc6c6cabd55
Marta Altimir <martaag18@gmail.com>
2025-02-11 12:20:03 +0100

    Upload environment filed

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     googleMapsApiKey: 'AIzaSyBoitwdYbHbH0BRwtcswB5lPVTrSR-ozQA',
-    apiUrl: process.env['NEXT_PUBLIC_API_URL'] || 'https://fallback-url.com'
+    apiUrl: "https://runners-app-production.up.railway.app/"
 };
